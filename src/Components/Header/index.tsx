5f0dc4061bd1757d0f05d7e1e9fe65beb9dbd27c
Elisio Wander <elisiowander@pop-os.localdomain>
2022-04-15 03:09:38 -0300

    desktop-header-styles

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,28 +1,47 @@
-import { useState } from 'react'
-import { BiMenuAltRight } from 'react-icons/bi'
-import { GetCoffeeButton } from '../GetCoffeeButton'
-import { MenuBurger } from '../MenuBurger'
+import { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { GetCoffeeButton } from "../GetCoffeeButton";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function Header() {
-  const [activeMenu, setActiveMenu] = useState(false)
+  const [activeMenu, setActiveMenu] = useState(false);
 
   function handleToggleActiveMenu() {
-    const isActive = !activeMenu
+    const isActive = !activeMenu;
 
-    setActiveMenu(isActive)
+    setActiveMenu(isActive);
   }
 
   return (
-    <header className={styles.header} >
-      <div className={styles.headerContent} >
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
         <img src="/assets/logo-mobile.svg" alt="Logo" />
 
         <BiMenuAltRight fontSize={35} onClick={handleToggleActiveMenu} />
 
-        <MenuBurger activeMenu={activeMenu} />
+        <nav className={`${styles.menuNav} ${activeMenu ? styles.active : ""}`}>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Menu</a>
+            </li>
+            <li>
+              <a href="/">Recompensas</a>
+            </li>
+            <li>
+              <a href="/">Gift Card</a>
+            </li>
+            <li>
+              <a href="/">Lojas</a>
+            </li>
+          </ul>
+        </nav>
+        
+        <GetCoffeeButton />
       </div>
     </header>
-  )
+  );
 }
