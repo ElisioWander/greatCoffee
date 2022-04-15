2e81783d8c284635389e1b831934c82f848c0316
Elisio Wander <elisiowander@pop-os.localdomain>
2022-04-15 19:05:08 -0300

    menu-toggle-transitions

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -18,27 +18,32 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/assets/logo-mobile.svg" alt="Logo" />
 
-        <BiMenuAltRight fontSize={35} onClick={handleToggleActiveMenu} />
+        <div className={styles.menuSection} >
+          <div className={`${styles.openMenuToggle} ${activeMenu ? styles.on : styles.openMenuToggle}`} onClick={handleToggleActiveMenu} >
+            <div></div>
+            <div></div>
+          </div>
 
-        <nav className={`${styles.menuNav} ${activeMenu ? styles.active : ""}`}>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Menu</a>
-            </li>
-            <li>
-              <a href="/">Recompensas</a>
-            </li>
-            <li>
-              <a href="/">Gift Card</a>
-            </li>
-            <li>
-              <a href="/">Lojas</a>
-            </li>
-          </ul>
-        </nav>
+          <nav className={`${styles.menuNav} ${activeMenu ? styles.active : ""}`}>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Menu</a>
+              </li>
+              <li>
+                <a href="/">Recompensas</a>
+              </li>
+              <li>
+                <a href="/">Gift Card</a>
+              </li>
+              <li>
+                <a href="/">Lojas</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         
         <GetCoffeeButton />
       </div>
