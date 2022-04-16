d4f079336dceaf31e3b245c777d31b7be9d6302c
elisioWander <elisio741@hotmail.com>
2022-04-15 21:46:23 -0300

    menu-toggle-completed

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -10,6 +10,8 @@ export function Dashboard() {
       <Main />
 
       <img className={styles.blurMobile} src="/assets/blur-mobile.png" alt="Blur mobile" />
+      <img className={styles.blurDesktopOne} src="/assets/blur-1.png" alt="First blur desktop" />
+      <img className={styles.blurDesktopTwo} src="/assets/blur-2.png" alt="Second blur desktop" />
     </div>
   );
 }
